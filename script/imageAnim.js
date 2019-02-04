ddef514e43d8d226903b888c4879d3e0f0185866
Wood <a_wood60810@tss-hr420-dm37.local>
2019-02-04 16:36:39 -0500

    drop functionality

--- script/imageAnim.js
+++ script/imageAnim.js
@@ -7,18 +7,53 @@
 		puzzleBoard = document.querySelector(".puzzle-board"),
 		puzzleSelectors = document.querySelectorAll("#buttonHolder img");
 
+	let dropZones = document.querySelectorAll('.drop-zone');
+
 	// functions go in the middle
 	function createPuzzlePieces(pictureIndex) {
 		// generate puzzle pieces for left hand side
 		// debugger;
 		pieces.forEach((piece, index) => {
-			let newPuzzlePieces = `<img id="piece${index}" class="puzzle-image" src="images/${piece + pictureIndex}.jpg" alt="thumbnail">`
+			let newPuzzlePieces = `<img draggable id="piece${index}" class="puzzle-image" src="images/${piece + pictureIndex}.jpg" alt="thumbnail">`
 
 			piecesBoard.innerHTML += newPuzzlePieces;
 		});
 
 		puzzleBoard.style.backgroundImage = `url(images/backGround${pictureIndex}.jpg)`
+
+		initDrag();
 	}
+
+	// drag n drop functionality goes here
+	
+	function initDrag() {
+		piecesBoard.querySelectorAll('img').forEach(img => {
+			img.addEventListener("dragstart", function(e) {
+				// e.preventDefault();
+				console.log('draggin...')
+
+				e.dataTransfer.setData("text/plain", this.id);
+			});
+		});
+	}
+
+	// handle dragover and drop
+	dropZones.forEach(zone => {
+		zone.addEventListener("dragover", function(e) {
+			e.preventDefault();
+			console.log('you dragged over me!');
+		});
+
+		zone.addEventListener("drop", function(e) {
+			e.preventDefault();
+			console.log('you dropped something on me');
+
+			let piece = e.dataTransfer.getData("text/plain");
+			e.target.appendChild(document.querySelector(`#${piece}`));
+		});
+	});
+
+
 
 	function resetPuzzlePieces() {
 		// empty the thumbnail container
